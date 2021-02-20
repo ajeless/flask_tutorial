883db9db19b2e8930620f9f0f81aa8af0f39436f
ajeless <ajeless@gmail.com>
2021-02-20 14:14:15 -0500

    Use port 80

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def product_page(product_id):
     return render_template('product-page.html', product_id=product_id)
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=80)
